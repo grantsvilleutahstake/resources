5beaf22ec49aa6cb50351ebd418299bec8ac18c0
Gregor Dzierzon <gregor@fullstackdevs.net>
2026-01-04 11:17:28 -0700

    added building name to speaking assignment

--- assets/js/pages/speaking-assignments.js
+++ assets/js/pages/speaking-assignments.js
@@ -25,6 +25,7 @@ class SpeakingAssignments {
   currentAssignments
   callings
   wards
+  buildings
 
   constructor() {
     this.init()
@@ -41,6 +42,7 @@ class SpeakingAssignments {
     this.assignments = await service.getSpeakingAssignments()
     this.callings = await service.getCallings()
     this.wards = await service.getWards()
+    this.buildings = await service.getBuildings()
     console.table(this.wards)
 
     yearDropdown.onSelectionChanged(this.selectedYearChanged)
@@ -144,10 +146,12 @@ class SpeakingAssignments {
       const auxilliary = this.callings.find(calling => calling.SpeakerId == assignment.AuxilliaryId)
       const highCouncilor = this.callings.find(calling => calling.SpeakerId == assignment.HighCouncilId)
       const ward = wards.find(ward => ward.Id == assignment.WardId)
+      const building = this.buildings.find(building => building.BuildingId == ward.BuildingId)
 
       const template = document.getElementById('ward-assignment-template').content.cloneNode(true)
       template.getElementById('ward-name').innerText = assignment.WardId
       template.getElementById('ward-time').innerText = ward.SacramentMeeting
+      template.getElementById('building-name').innerText = `( ${building.Name} )`
       template.getElementById('auxilliary-name').innerText = `${auxilliary.Name} (${auxilliary.SpeakingAbbreviation})`
       template.getElementById('high-council-name').innerText = `${highCouncilor.Name} (${highCouncilor.SpeakingAbbreviation})`
 
--- assets/js/pages/ward-conferences.js
+++ assets/js/pages/ward-conferences.js
@@ -15,6 +15,7 @@ class WardConferencePage {
 
   selectedWardId = 'All'
   selectedYear = 0
+  wards
 
   constructor()
   {
@@ -53,6 +54,9 @@ class WardConferencePage {
 
   displayWardConferences = async () => {
     let meetings = await service.getWardConferences()
+    let wards = await service.getWards()
+    let buildings = await service.getBuildings()
+
     if (this.selectedWardId != 'All') {
       meetings = meetings.filter(row => {
           return row.WardId == this.selectedWardId
@@ -69,9 +73,14 @@ class WardConferencePage {
 
     meetings.forEach(row => {
 
+      const ward = wards.find(ward => ward.Id == row.WardId)
+      const building = buildings.find(building => building.BuildingId == ward.BuildingId)
+
       const div = document.getElementById('ward-conference-row-template').content.cloneNode(true)
 
       div.querySelector('.ward-conference-name').innerText = row.WardId
+      div.querySelector('.ward-time').innerText = ward.SacramentMeeting
+      div.querySelector('.building-name').innerText = building.Name
       div.querySelector('.ward-conference-date').innerText = `${row.Month} ${row.Day}`
       div.querySelector('.ward-conference-speaker').innerText = row.Speaker
 
