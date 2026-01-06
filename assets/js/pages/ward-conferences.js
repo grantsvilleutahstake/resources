2a9107f32c7edf017145a742b8ea66e81ae7caf0
Gregor Dzierzon <gregor@fullstackdevs.net>
2026-01-06 09:55:41 -0700

    fixed ward conference error

--- assets/js/pages/ward-conferences.js
+++ assets/js/pages/ward-conferences.js
@@ -57,28 +57,27 @@ class WardConferencePage {
     let wards = await service.getWards()
     let buildings = await service.getBuildings()
 
+    wards = wards.filter(ward => ward.Year == this.selectedYear)
+
+    meetings = meetings.filter(row => row.Year == this.selectedYear || this.selectedYear == 0)
     if (this.selectedWardId != 'All') {
       meetings = meetings.filter(row => {
           return row.WardId == this.selectedWardId
       })  
     }
 
-    meetings = meetings.filter(row => row.Year == this.selectedYear || this.selectedYear == 0)
-
-    console.table(meetings);
-    
 
     const parent = document.getElementById('ward-conference-table')
     parent.innerText = ''
 
     meetings.forEach(row => {
 
-      const ward = wards.find(ward => ward.Id == row.WardId)
+      const ward = wards.find(ward => ward.Id == row.WardId && ward.Year == this.selectedYear )
       const building = buildings.find(building => building.BuildingId == ward.BuildingId)
 
       const div = document.getElementById('ward-conference-row-template').content.cloneNode(true)
 
-      div.querySelector('.ward-conference-name').innerText = row.WardId
+      div.querySelector('.ward-conference-name').innerText = ward.Name
       div.querySelector('.ward-time').innerText = ward.SacramentMeeting
       div.querySelector('.building-name').innerText = building.Name
       div.querySelector('.ward-conference-date').innerText = `${row.Month} ${row.Day}`
